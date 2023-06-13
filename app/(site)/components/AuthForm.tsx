fbcc94ac48755020f2ba78a80974772b66122fab
Neil Rowland <neilrowland56@gmail.com>
2023-06-13 14:22:20 +0100

    Added Input and Button components to AuthForm

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Button from '@/app/components/Button'
 import Input from '@/app/components/inputs/Input'
 import { useCallback, useState } from 'react'
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form'
@@ -13,7 +14,7 @@ Also serves as a faster & smaller drop-in replacement for the classnames module.
 
 const AuthForm = () => {
   const [variant, setVariant] = useState<Variant>('LOGIN')
-  const [loading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false)
 
   const toggleVariant = useCallback(() => {
     if (variant === 'LOGIN') {
@@ -57,13 +58,39 @@ const AuthForm = () => {
     <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
       <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
         <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
+          {variant === 'REGISTER' && (
+            <Input
+              disabled={isLoading}
+              register={register}
+              errors={errors}
+              required
+              id='name'
+              label='Name'
+            />
+          )}
           <Input
+            disabled={isLoading}
             register={register}
             errors={errors}
             required
-            id='name'
-            label='Name'
+            id='email'
+            label='Email address'
+            type='email'
           />
+          <Input
+            disabled={isLoading}
+            register={register}
+            errors={errors}
+            required
+            id='password'
+            label='Password'
+            type='password'
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type='submit'>
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+            </Button>
+          </div>
         </form>
       </div>
     </div>
--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -6,6 +6,7 @@ import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form'
 interface InputProps {
   label: string
   id: string
+
   type?: string
   required?: boolean
   register: UseFormRegister<FieldValues>
@@ -56,7 +57,7 @@ const Input: React.FC<InputProps> = ({
             ring-1 
             ring-inset 
             ring-gray-300 
-            placeholder:text-gray-400 
+            placeholder:text-blue-300
             focus:ring-2 
             focus:ring-inset 
             focus:ring-sky-600 
