30ade01f7c6762eb0917fad9a733fa299def4654
Neil Rowland <neilrowland56@gmail.com>
2023-06-13 07:01:25 +0100

    Added Input component

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -57,7 +57,13 @@ const AuthForm = () => {
     <div className='mt-8 sm:mx-auto sm:w-full sm:max-w-md'>
       <div className='bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10'>
         <form className='space-y-6' onSubmit={handleSubmit(onSubmit)}>
-          <Input />
+          <Input
+            register={register}
+            errors={errors}
+            required
+            id='name'
+            label='Name'
+          />
         </form>
       </div>
     </div>
